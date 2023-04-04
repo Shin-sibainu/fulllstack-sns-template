15274e38b22f7d906fc8dcdc5dde7180aa219b49
Shin-sibainu <shincode0712@gmail.com>
2023-04-04 14:45:29 +0900

    done

---
+++ Navbar.tsx
@@ -0,0 +1,22 @@
+<header className="bg-gray-700 p-4 text-white">
+  <div className="container mx-auto flex justify-between items-center">
+    <h1 className="font-semibold text-xl">
+      <Link href="/">SNS Clone</Link>
+    </h1>
+    <nav>
+      <ul className="flex space-x-4">
+        {user ? (
+          <>
+            <Link href="/">ホーム</Link>
+            <button onClick={logout}>ログアウト</button>
+          </>
+        ) : (
+          <>
+            <Link href="/login">ログイン</Link>
+            <Link href="/signup">サインアップ</Link>
+          </>
+        )}
+      </ul>
+    </nav>
+  </div>
+</header>;
